4c5cc0ab11e7de038f51d6ed70cae45cbe1e628a
Derpimort <jatinsaini580@gmail.com>
2020-05-31 21:32:28 +0530

    Add regex funcs from ipynb and preprocess complete.

--- data_clean.py
+++ data_clean.py
@@ -12,7 +12,72 @@ DATA_DIR="data/"
 with open(DATA_DIR+"Train_data.csv", "r") as f:
         print(f.readline().strip("\n"))
 
-badf=open(DATA_DIR+"bad_rows_simple.csv", "w", newline='')
+
+#checks=["'", '"']
+checks=['"']
+def unbalanced(s):
+    for check in checks:
+        if (s.count(check)-s.count(f'\\{check[0]}'))%2!=0:
+            return True
+    return False
+def lineCleaner(line):
+    line=line.strip("\n")
+    splitl=line.split("|")
+    slen=len(splitl)
+    ids=re.findall(r'\|([0-9]+)$',line)
+    if slen==5:
+        return splitl
+    else:
+        try:
+            lindex=0
+            res=[]
+            while(lindex<slen):
+                if unbalanced(splitl[lindex]) and lindex<slen-1:
+                    rindex=lindex+1
+                    while not unbalanced(splitl[rindex]) and rindex<slen-2:
+                        rindex+=1
+                    res+=[" ".join(splitl[lindex:rindex+1])]
+                    lindex=rindex
+                else:
+                    res+=[splitl[lindex]]
+                lindex+=1
+#             if len(res)==6:
+#                 res[3]=res[3]+res[4]
+#                 res[]
+            if len(res)>5:
+                res[3]=" ".join(res[3:])
+                res=res[:4]
+            if len(res)==4:
+                res+=[ids[0].strip("|")]
+#             if len(res)==3:
+#                 long_desc=re.search(r'\|([^\|]*)\|[0-9]*$', line)
+            if len(res)!=5:
+                res=[]
+                url=re.search(r'\|http.*?\|', line)
+                if url is not None:
+                    res+=[line[:url.start()]]
+                    res+=[line[url.start():url.end()]]
+                else:
+                    res+=[None, None]
+                long_desc=re.search(r'\|([^\|]*)\|[0-9]*$', line)
+                if long_desc is not None:
+                    res+=[line[url.end():long_desc.start()]]
+                    res+=[line[long_desc.start():long_desc.end()]]
+                else:
+                    res+=[None, None]
+                res+=[ids[0]]
+                for i in range(5):
+                    if res[i] is not None:
+                        res[i]=res[i].strip("|")
+            return res
+        except Exception as e:
+            return [None]*5
+#             res=[ids[0].strip("|")]
+#             url=re.search(r'\|http.*?\|', line).start()
+            
+#             long_desc=re.search(r'\|([^\|]*)\|[0-9]*$', line)
+
+badf=open(DATA_DIR+"bad_rows.csv", "w", newline='')
 bad_csv_writer=csv.writer(badf, delimiter="|", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
 output = StringIO()
 csv_writer = csv.writer(output, delimiter=u'\u0001')
@@ -25,16 +90,22 @@ with open(DATA_DIR+"Train_data.csv", "r") as f:
     rc=csv.reader(f, delimiter="|", quotechar='"')
     for row in tqdm(rc, total=1411103):
         if len(row)!=5:
-            bad_count+=1
-            bad_csv_writer.writerow(row)
-        else:
-            csv_writer.writerow(row)
+            try:
+                row_new=lineCleaner("|".join(row))
+                int(row_new[-1])
+            except Exception as e:
+                bad_count+=1
+                bad_csv_writer.writerow(row)
+                row_new=[None, None, None, None, row[-1]]
+            finally:
+                row=row_new
+        csv_writer.writerow(row)
         
 
 output.seek(0)
 df_first = pd.read_csv(output, sep=u'\u0001')
 print(df_first.head())
 
-df_first.to_csv(DATA_DIR+"train_cleaned_simple.csv", sep=u'\u0001', index=False)
-df_first.to_pickle(DATA_DIR+"train_cleaned_simple.pkl")
+df_first.to_csv(DATA_DIR+"train_cleaned.csv", sep=u'\u0001', index=False)
+df_first.to_pickle("train_cleaned.pkl")
 print("Bad files", bad_count)
